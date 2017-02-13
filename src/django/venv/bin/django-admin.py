764ecab9734e7385132c1c86d27e794d0b96ce8e
jhnoor <jamawadi@gmail.com>
2017-02-13 03:10:27 +0100

    first commit

---
+++ src/django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jamawadinoor/Programming/docker-nginx-django-angular2/src/django/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
