21d446125ecec6bed22a1bd44ac3832456ce07c9
jhnoor <jamawadi@gmail.com>
2017-02-15 12:46:07 +0100

    delte all pyc files, added documentation of hierarchy

---
+++ README/README.md
@@ -0,0 +1,19 @@
+# Setup
+     
+* Get [docker](https://www.docker.com/products/overview).
+* Confirm that docker is installed (type 'docker' and 'docker-compose', if they both work you're good to go!)
+* Clone the project, cd into the project directory.
+* Build the docker containers:
+```docker-compose build```
+* Bring the containers up:
+```docker-compose up```
+
+If everything worked you should be able to see the angular application at [127.0.0.1](http://127.0.0.1).
+
+# Hierarchy
+![docker_diagram](jhnoor.github.com/fg/README/docker_diagram.png)
+
+
+# TODO
+
+Check the [trello board](https://trello.com/b/tbU3wIZc/fg-3-0)
--- src/django_backend/fg/api/models.py
+++ src/django_backend/fg/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+class Category(models.Model):
+    category = models.CharField(max_length=80)
+
+
+class Media(models.Model):
+    medium = models.CharField(max_length=80)
+
+class Album(models.Model):
+    name = models.CharField(max_length=5)
+
+class Place(models.Model):
+    place = models.CharField(max_length=80)
+
+class ImageModel(models.Model):
+    image_prod = models.ImageField()
+    image_web = models.ImageField()
+    image_thumb = models.ImageField()
+
+    # Foreign keys
+    tag = models.ForeignKey(Tag)
+    category = models.ForeignKey(Category)
+    media = models.ForeignKey(Media)
+    album = models.ForeignKey(Album)
+    place = models.ForeignKey(Place)
