0215e82b24c48879cc262e1c9d7c9775abf5f47f
jhnoor <jamawadi@gmail.com>
2017-02-23 12:16:26 +0200

    migrations, new image model

--- src/django_backend/fg/api/models.py
+++ src/django_backend/fg/api/models.py
@@ -1,20 +1,42 @@
 from django.db import models
+from datetime import datetime
+from django.utils.timezone import now
 
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):
     category = models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.category
+
 
 class Media(models.Model):
     medium = models.CharField(max_length=80, unique=True)
 
+    def __str__(self):
+        return self.medium
+
+
 class Album(models.Model):
     name = models.CharField(max_length=5, unique=True)
 
+    def __str__(self):
+        return self.name;
+
+
 class Place(models.Model):
     place = models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.place;
+
 
 class Image(models.Model):
     #image_prod = models.ImageField()
@@ -27,3 +49,5 @@ class Image(models.Model):
     media = models.ForeignKey(Media)
     album = models.ForeignKey(Album)
     place = models.ForeignKey(Place)
+    date_taken = models.DateTimeField(default=now)
+    date_modified = models.DateTimeField()
