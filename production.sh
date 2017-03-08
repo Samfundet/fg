c0afb5e3596bd4898b03974c08792783c7dc0cfb
jama <jamawadi@gmail.com>
2017-03-08 18:58:43 +0000

    lets try this again [ci skip]

--- production.sh
+++ production.sh
@@ -5,15 +5,23 @@ colorYellow() { echo $(tput setaf 3); }
 colorGreen() { echo $(tput setaf 2); }
 colorRed() { echo $(tput setaf 1); }
 
-docker-compose stop
-docker-compose build
+colorRed
+echo "docker-compose -f docker-compose.prod.yml stop"
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
+
+colorYellow
+echo "docker-compose -f build"
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
+
+colorGreen
+echo "docker-compose up -fd"
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 
 colorYellow
-docker-compose ps
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml ps
 
 if [ $? -eq 0 ]
-then
+ithen
   colorGreen
   echo "Successfully built and upped docker containers"
 else
