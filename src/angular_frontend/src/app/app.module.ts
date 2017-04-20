a16f85f94450686aa734d104b2a713969d6df5f8
jhnoor <jamawadi@gmail.com>
2017-04-21 00:05:49 +0200

    added primeng

--- src/angular_frontend/src/app/app.module.ts
+++ src/angular_frontend/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { InputTextModule, ButtonModule } from 'primeng/primeng';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,8 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    InputTextModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
