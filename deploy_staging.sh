8220336f88c203123ac499631a0555679e53e5c6
jhnoor <jamawadi@gmail.com>
2017-04-29 23:03:43 +0200

    obv need permissions

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-LOGFILE="/var/log/travis-deploys.log"
+LOGFILE="travis-deploys.log"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 ssh fg@146.185.181.250 "cd fg && git pull && bash production.sh && echo $TIMESTAMP >> $LOGFILE"
