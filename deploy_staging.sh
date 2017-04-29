28b0cacccef2b135d2a5a0969da29bc31f63ca40
jhnoor <jamawadi@gmail.com>
2017-04-29 22:57:03 +0200

    lets see now

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-ssh fg@146.185.181.250 "cd fg && git pull && bash production.sh"
+LOGFILE="/var/log/travis-deploys.log"
+TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+ssh fg@146.185.181.250 "cd fg && git pull && bash production.sh && echo $TIMESTAMP >> $LOGFILE"
