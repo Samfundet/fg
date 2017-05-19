adcfb9571d242b131bbad46a81bc86c52ec191af
Jama Noor <jamawadi@gmail.com>
2017-05-19 15:54:02 +0200

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 LOGFILE="travis-deploys.log"
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
-ssh fg@146.185.181.250 "cd fg && git pull && bash development.sh && echo $TIMESTAMP >> $LOGFILE"
+ssh fg@146.185.181.250 "cd fg && git reset --hard && git pull && bash development.sh && echo $TIMESTAMP >> $LOGFILE"
