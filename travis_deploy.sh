a5ee04eddf6d38a8f0d7a08637ffc9b9f1304893
jhnoor <jamawadi@gmail.com>
2017-10-01 23:15:18 +0200

    test

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -2,4 +2,5 @@
 set -x
 
 ssh fg@fgbeta.samfundet.no 'bash -s' < $TRAVIS_BUILD_DIR/deploy_script.sh
+pwd
 rsync -r $TRAVIS_BUILD_DIR/src/angular_frontend/dist fg@fgbeta.samfundet.no:./fg/src/angular_frontend/
