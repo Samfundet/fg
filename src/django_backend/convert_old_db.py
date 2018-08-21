c9265abdd661b923777ddb769a701511392d44bd
Jama Noor <jamawadi.noor@acando.no>
2018-08-21 22:46:16 +0200

    latest image and page number

--- src/django_backend/convert_old_db.py
+++ src/django_backend/convert_old_db.py
@@ -6,8 +6,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fg.settings")
 import django
 django.setup()
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from django import db
-from fg.api import models
+from fg.api import models, views
 from fg.legacy import models as old_models
 from fg.fg_auth.models import User
 from versatileimagefield.fields import VersatileImageField
@@ -98,58 +100,131 @@ def convert_Place():
     models.Place.objects.bulk_create(obj_list)
 
 
+def get_latest_image_number_and_page_number(album_pk):
+    latest_page = models.Photo.objects.filter(
+        album=album_pk
+    ).aggregate(db.models.Max('page')).page__max
+
+    latest_image_number = models.Photo.objects.filter(
+        album=album_pk, page=latest_page
+    ).aggregate(db.models.Max('image_number')).image_number__max
+
+    new_image_number = latest_image_number + 1
+    new_page = latest_page
+    if new_image_number > 99 and new_page < 99:
+        new_image_number = 1
+        new_page = latest_page + 1
+    else:
+        return False
+
+    return {
+        'new_image_number': new_image_number,
+        'new_page': new_page
+    }
+
+
 def convert_Photo():
     print("Converting Photos")
     chunksize = 1000
-    old_photo_set = old_models.ArchiveImagemodel.objects.using('old_db').order_by('pk')
+    queryset = old_models.ArchiveImagemodel.objects.using('old_db')
 
     key_dict = {}
     dupes = []
-    # obj_list = []
+    pk = 0
+    last_pk = queryset.order_by('-pk')[0].pk
+    old_photo_set = queryset.order_by('pk')
 
-    for item in old_photo_set.iterator():
-        key = '{}#{}-{}-{}'.format(item.pk, item.page, item.image_number, item.album.pk)
-        if key in key_dict:
-            dupes.append(key)
+    while pk < last_pk:
+        obj_list = []
+
+        for item in old_photo_set.filter(pk__gt=pk)[:chunksize]:
+            sys.stdout.write(".")
+            pk = item.pk
+
+            key = '{}-{}-{}'.format(item.page,
+                                    item.image_number, item.album.pk)
+            if key in key_dict:
+                dupes.append(item)
+                continue
+            else:
+                key_dict[key] = item
+
+            if len(item.motive) >= 256:
+                item.description = item.motive
+                item.motive = item.motive[:255]
+
+            new_item = models.Photo(
+                pk=item.pk,
+                photo=item.image_prod,
+                motive=item.motive,
+                scanned=item.scanned,
+                on_home_page=item.on_home_page,
+                lapel=item.lapel,
+                splash=False,
+                page=item.page,
+                image_number=item.image_number,
+                date_taken=item.date,
+
+                security_level=models.SecurityLevel.objects.get(
+                    pk=item.security_level.pk),
+                category=models.Category.objects.get(pk=item.category.pk),
+                media=models.Media.objects.get(pk=item.media.pk),
+                album=models.Album.objects.get(pk=item.album.pk),
+                place=models.Place.objects.get(pk=item.place.pk)
+            )
+            obj_list.append(new_item)
+
+        print("#")
+        print("Bulk inserting {} photos".format(len(obj_list)))
+        models.Photo.objects.bulk_create(obj_list)
+
+    print("{} duplicates found".format(len(dupes)))
+    left_over_dupes = []
+    for item in dupes:
+        resp = get_latest_image_number_and_page_number(item.album.pk)
+        if resp:
+            new_item = models.Photo(
+                pk=item.pk,
+                photo=item.image_prod,
+                motive=item.motive,
+                scanned=item.scanned,
+                on_home_page=item.on_home_page,
+                lapel=item.lapel,
+                splash=False,
+                page=resp.new_page,
+                image_number=resp.new_image_number,
+                date_taken=item.date,
+
+                security_level=models.SecurityLevel.objects.get(
+                    pk=item.security_level.pk),
+                category=models.Category.objects.get(pk=item.category.pk),
+                media=models.Media.objects.get(pk=item.media.pk),
+                album=models.Album.objects.get(pk=item.album.pk),
+                place=models.Place.objects.get(pk=item.place.pk)
+            )
         else:
-            key_dict[key] = item
-    print(dupes)
+            left_over_dupes.append(item)
+        print(resp)
+        # For item album.pk, get highest page and image number
+        # store in db with that
+        # ???
+        # profit
+        pass
 
 
-    # for item in old_Photos[:100]:
+def attach_Tags_to_photos():
+    old_tag2photo_set = old_models.ArchiveImagemodelTag.objects.using(
+        'old_db').order_by('imagemodel')
 
-    #     key = '{}-{}-{}'.format(item.page, item.image_number, item.album.pk)
-    #     if key in key_dict:
-    #         dupes.append(key)
-    #         item.page = 99
-    #         item.image_number = 99
-    #     else:
-    #         key_dict[key] = item
+    for item in old_tag2photo_set:
+        try:
+            photo = models.Photo.objects.get(pk=item.imagemodel.pk)
+            tag = models.Tag.objects.get(pk=item.tag.pk)
 
-    #     new_item = models.Photo(
-    #         photo=item.image_prod,
-    #         motive=item.motive,
-    #         scanned=item.scanned,
-    #         on_home_page=item.on_home_page,
-    #         lapel=item.lapel,
-    #         splash=False,
-    #         page=item.page,
-    #         image_number=item.image_number,
-    #         date_taken=item.date,
-
-    #         security_level=models.SecurityLevel.objects.get(
-    #             pk=item.security_level.pk),
-    #         category=models.Category.objects.get(pk=item.category.pk),
-    #         media=models.Media.objects.get(pk=item.media.pk),
-    #         album=models.Album.objects.get(pk=item.album.pk),
-    #         place=models.Place.objects.get(pk=item.place.pk)
-    #     )
-    #     obj_list.append(new_item)
-
-    # print("Bulk insert photos")
-    # models.Photo.objects.bulk_create(obj_list)
-
-    # print(dupes)
+            photo.tags.add(tag)
+            photo.save()
+        except ObjectDoesNotExist:
+            continue
 
 
 def convert():
@@ -160,6 +235,7 @@ def convert():
     convert_Media()
     convert_Place()
     convert_Photo()
+    attach_Tags_to_photos()
 
     User.objects.create_superuser(
         username='fg', email='', password='qwer1234')
